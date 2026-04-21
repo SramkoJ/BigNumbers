d5a296b71e80a1b81eca0d7a9ab5ed8ea97ba7bf
Jakub Šramko <jakub.sramko@student.tuke.sk>
2026-04-21 18:41:22 +0200

    Edit NumberGenerator notes, add corresponding tests and notes

--- src/NumberGenerator.java
+++ src/NumberGenerator.java
@@ -3,12 +3,19 @@ import java.util.Random;
 public class NumberGenerator {
     private final Random random = new Random();
 
+    /**
+     * Generates a BigNumber with a given number of digits.
+     */
     public BigNumber generate(int length) {
+        if (length < 1) throw new IllegalArgumentException("Length must be > 0");
         int[] digits = new int[length];
-        digits[length - 1] = random.nextInt(9) + 1;
+        // Second to last digit are 0-9
         for (int i = 0; i < length - 1; i++) {
             digits[i] = random.nextInt(10);
         }
+        // First digit cant be zero
+        digits[length - 1] = random.nextInt(9) + 1;
+        // Generating if number is positive or negative
         boolean isNegative = random.nextBoolean();
         return new BigNumber(digits, isNegative);
     }
--- test/NumberGeneratorTest.java
+++ test/NumberGeneratorTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.RepeatedTest;
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,16 +7,16 @@ class NumberGeneratorTest {
     private final NumberGenerator generator = new NumberGenerator();
 
     @Test
-    @DisplayName("Should generate array with given length")
     void testLength() {
+        //Should generate array with given length
         int expectedLength = 50;
         BigNumber result = generator.generate(expectedLength);
         assertEquals(expectedLength, result.getDigits().length);
     }
 
     @RepeatedTest(10)
-    @DisplayName("First digit cant be zero")
     void testFirstDigitNotZero() {
+        //First digit cant be zero
         BigNumber result = generator.generate(5);
         int[] digits = result.getDigits();
         int mostSignificantDigit = digits[digits.length - 1];
@@ -26,10 +25,22 @@ class NumberGeneratorTest {
     }
 
     @Test
-    @DisplayName("Should properly generate single digit number")
     void testSingleDigit() {
+        //Should properly generate single digit number
         BigNumber result = generator.generate(1);
         assertEquals(1, result.getDigits().length);
         assertTrue(result.getDigits()[0] >= 1 && result.getDigits()[0] <= 9);
+    }
+
+    @Test
+    void testZeroLength() {
+        // IllegalArgumentException expected when length is 0
+        assertThrows(IllegalArgumentException.class, () -> generator.generate(0), "IllegalArgumentException was thrown");
+    }
+
+    @Test
+    void testNegativeLength() {
+        // IllegalArgumentException also expected when length is 0
+        assertThrows(IllegalArgumentException.class, () -> generator.generate(-5), "IllegalArgumentException was thrown");
     }
 }
